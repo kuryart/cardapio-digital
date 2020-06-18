f0ffe73b51ac0abd8553ca550898950b155fb6e5
kurya <kuryart1@gmail.com>
2020-06-18 19:36:28 -0300

    Estrutura de banco de dados alterada
    A estrutura das tabelas foram alteradas de acordo com o arquivo
    etc/db_tables.ods.

---
+++ src/cardapio-digital/database/migrations/2020_06_18_214246_create_produto_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('produto', function (Blueprint $table)
+      {
+        $table->increments('id');
+        $table->string('nome')->unique();
+        $table->integer('categoria_id')->unsigned();
+        $table->foreign('categoria_id')->references('id')->on('categoria')->onDelete('cascade')->onUpdate('cascade');
+        $table->string('descricao_resumida');
+        $table->string('descricao_completa');
+        $table->decimal('preco', 5, 2);
+        $table->string('foto_url')->nullable();
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
