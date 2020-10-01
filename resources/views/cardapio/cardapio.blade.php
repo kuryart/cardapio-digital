c0be7221ad89012ca80ca2ad6ee90202169af6e7
kuryart <kuryart1@gmail.com>
2020-09-30 22:44:29 -0300

    Correção de layout do cardápio.

--- resources/views/cardapio/cardapio.blade.php
+++ resources/views/cardapio/cardapio.blade.php
@@ -42,40 +42,210 @@
                       <div class="card-body categoria-card-body">
                         @foreach ($produtos as $produto)
                           @if ($produto->categoria_id === $categoria->id)
-                          <!-- Produto - BEGIN -->
-                          <div class="container-fluid produto-container">
-                            <div class="row produto-main-wrapper">
-                              <div class="col-12 col-sm-12 col-md	col-lg col-xl produto-detalhes">
-                                <div class="row produto-header">
-                                  <div class="col-auto produto-title-wrapper">
-                                    <span class="produto-title">{{ $produto->nome }}</span>
+                            @if (count($produto->precos) === 1)
+                              <!-- Produto - BEGIN -->
+                              <div class="container-fluid produto-container">
+                                <div class="row produto-main-wrapper align-items-center">
+                                  <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 produto-detalhes">
+                                    <div class="row produto-header">
+                                      <div class="col-auto produto-title-wrapper">
+                                        <span class="produto-title">{{ $produto->nome }}</span>
+                                      </div>
+                                    </div>
+                                    <div class="row produto-body">
+                                      <div class="col produto-descricao-wrapper">
+                                        <span class="produto-descricao">{{ $produto->descricao }}</span>
+                                      </div>
+                                    </div>
                                   </div>
-                                </div>
-                                <div class="row produto-body">
-                                  <div class="col produto-descricao-wrapper">
-                                    <span class="produto-descricao">{{ $produto->descricao }}</span>
+
+                                  <!-- <div class="col-10 col-sm-10 produto-preco-column-adjust">
+
+                                  </div> -->
+
+                                  <div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 produto-precos">
+
                                   </div>
-                                </div>
-                              </div>
-                              <div class="col-12 col-sm-12 col-md-auto col-lg-auto col-xl-auto produto-precos">
-                                <div class="row produto-precos-legendas justify-content-center">
-                                  @foreach ($produto->precos as $precos)
-                                    <div class="col-3 col-sm-3 col-md col-lg col-xl produto-preco-legenda-wrapper">
-                                      <span class="produto-preco-legenda">{{ $precos['legenda'] }}</span>
+
+
+                                  @foreach ($produto->precos as $preco)
+                                    <div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 produto-precos">
+                                      <div class="row justify-content-center">
+                                        <div class="col">
+                                          <span class="produto-preco-legenda">{{ $preco['legenda'] }}</span>
+                                        </div>
+                                      </div>
+                                      <div class="row justify-content-center">
+                                        <div class="col">
+                                          <span class="produto-preco-valor">{{ $preco['valor'] }}</span>
+                                        </div>
+                                      </div>
                                     </div>
                                   @endforeach
-                                </div>
-                                <div class="row produto-precos-valores justify-content-center">
-                                  @foreach ($produto->precos as $precos)
-                                    <div class="col-3 col-sm-3 col-md col-lg col-xl produto-preco-legenda-wrapper">
-                                      <span class="produto-preco-valor">{{ $precos['valor'] }}</span>
+
+
+                                    <!-- <div class="row justify-content-right">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col">
+                                          <div class="row justify-content-center">
+                                            <div class="col">
+                                              <span class="">Legenda1</span>
+                                            </div>
+                                          </div>
+                                          <div class="row justify-content-center">
+                                            <div class="col">
+                                              <span class="">Valor1</span>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      @endforeach
+                                    </div> -->
+
+                                    <!-- <div class="row produto-precos-legendas justify-content-center">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 produto-preco-legenda-wrapper">
+                                          <span class="produto-preco-legenda">{{ $preco['legenda'] }}</span>
+                                        </div>
+                                      @endforeach
                                     </div>
-                                  @endforeach
+                                    <div class="row produto-precos-valores justify-content-center">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 produto-preco-legenda-wrapper">
+                                          <span class="produto-preco-valor">{{ $preco['valor'] }}</span>
+                                        </div>
+                                      @endforeach
+                                    </div> -->
+
+
                                 </div>
                               </div>
-                            </div>
-                          </div>
-                          <!-- Produto - END -->
+                              <!-- Produto - END -->
+                            @elseif (count($produto->precos) === 2)
+                              <!-- Produto - BEGIN -->
+                              <div class="container-fluid produto-container">
+                                <div class="row produto-main-wrapper">
+                                  <div class="col-12 col-sm-12 col-md-8 col-lg-8 col-xl-8 produto-detalhes">
+                                    <div class="row produto-header">
+                                      <div class="col-auto produto-title-wrapper">
+                                        <span class="produto-title">{{ $produto->nome }}</span>
+                                      </div>
+                                    </div>
+                                    <div class="row produto-body">
+                                      <div class="col produto-descricao-wrapper">
+                                        <span class="produto-descricao">{{ $produto->descricao }}</span>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <!-- <div class="col-8 col-sm-8 produto-preco-column-adjust">
+
+                                  </div> -->
+
+                                  @foreach ($produto->precos as $preco)
+                                    <div class="col-6 col-sm-6 col-md-2 col-lg-2 col-xl-2 produto-precos">
+                                      <div class="row justify-content-center">
+                                        <div class="col">
+                                          <span class="produto-preco-legenda">{{ $preco['legenda'] }}</span>
+                                        </div>
+                                      </div>
+                                      <div class="row justify-content-center">
+                                        <div class="col">
+                                          <span class="produto-preco-valor">{{ $preco['valor'] }}</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  @endforeach
+
+
+
+
+                                    <!-- <div class="row justify-content-right">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col">
+                                          <div class="row justify-content-center">
+                                            <div class="col">
+                                              <span class="">Legenda1</span>
+                                            </div>
+                                          </div>
+                                          <div class="row justify-content-center">
+                                            <div class="col">
+                                              <span class="">Valor1</span>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      @endforeach
+                                    </div> -->
+
+                                    <!-- <div class="row produto-precos-legendas justify-content-center no-gutters">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col-3 col-sm-3 col-md col-lg col-xl produto-preco-legenda-wrapper">
+                                          <span class="produto-preco-legenda">{{ $preco['legenda'] }}</span>
+                                        </div>
+                                      @endforeach
+                                    </div>
+                                    <div class="row produto-precos-valores justify-content-center no-gutters">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col-3 col-sm-3 col-md col-lg col-xl produto-preco-legenda-wrapper">
+                                          <span class="produto-preco-valor">{{ $preco['valor'] }}</span>
+                                        </div>
+                                      @endforeach
+                                    </div> -->
+
+                                </div>
+                              </div>
+                              <!-- Produto - END -->
+                            @elseif (count($produto->precos) === 3)
+                              <!-- Produto - BEGIN -->
+                              <div class="container-fluid produto-container">
+                                <div class="row produto-main-wrapper">
+                                  <div class="col-12 col-sm-12 col-md-9	col-lg-9 col-xl-9 produto-detalhes">
+                                    <div class="row produto-header">
+                                      <div class="col-auto produto-title-wrapper">
+                                        <span class="produto-title">{{ $produto->nome }}</span>
+                                      </div>
+                                    </div>
+                                    <div class="row produto-body">
+                                      <div class="col produto-descricao-wrapper">
+                                        <span class="produto-descricao">{{ $produto->descricao }}</span>
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  @foreach ($produto->precos as $preco)
+                                    <div class="col-12 col-sm-12 col-md-1 col-lg-1 col-xl-1 produto-precos">
+                                      <div class="row justify-content-center">
+                                        <div class="col">
+                                          <span class="">Legenda1</span>
+                                        </div>
+                                      </div>
+                                      <div class="row justify-content-center">
+                                        <div class="col">
+                                          <span class="">Valor1</span>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  @endforeach
+
+                                  <!-- <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-4 produto-precos">
+                                    <div class="row produto-precos-legendas justify-content-center">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col-3 col-sm-3 col-md col-lg col-xl produto-preco-legenda-wrapper">
+                                          <span class="produto-preco-legenda">{{ $preco['legenda'] }}</span>
+                                        </div>
+                                      @endforeach
+                                    </div>
+                                    <div class="row produto-precos-valores justify-content-center">
+                                      @foreach ($produto->precos as $preco)
+                                        <div class="col-3 col-sm-3 col-md col-lg col-xl produto-preco-legenda-wrapper">
+                                          <span class="produto-preco-valor">{{ $preco['valor'] }}</span>
+                                        </div>
+                                      @endforeach
+                                    </div>
+                                  </div> -->
+                                </div>
+                              </div>
+                              <!-- Produto - END -->
+                            @endif
                           @endif
                         @endforeach
                       </div>
