e5319bab59718035954c6245a519eb52ac6c3b01
kuryart <kuryart1@gmail.com>
2020-10-13 20:18:33 -0300

    Adição de cores ao layout das seções e categorias

--- resources/views/admin/dashboard/head.blade.php
+++ resources/views/admin/dashboard/head.blade.php
@@ -12,7 +12,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css2?family=Lalezar&family=Passion+One&family=Tangerine:wght@700&family=Signika&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Lalezar&family=Passion+One&family=Tangerine:wght@700&family=Signika&family=Caveat:wght@700&display=swap" rel="stylesheet">
 <!-- === Stylesheets === -->
 <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
 <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
